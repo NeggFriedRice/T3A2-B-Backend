507b7dd492ffa9be852a118844b13310c7e677a5
Finley <trinity.tringl@gmail.com>
2024-02-19 00:56:51 +1000

    feat: event routes created

--- app.js
+++ app.js
@@ -8,4 +8,36 @@ app.get('/', (req, res) => {
     res.send('Hello, World!')
 })
 
+/* Event Routes */
+
+// List of all events
+app.get('/events', (req, res) => {
+    // TODO: Create Functionality
+    res.send('List of events')
+})
+
+// Get a single event
+app.get('/events/:id', (req, res) => {
+    // TODO: Create Functionality
+    res.send('Get an event')
+})
+
+// Create an event
+app.post('/events', (req, res) => {
+    // TODO: Create Functionality
+    res.send('Create an event')
+})
+
+// Update an event
+app.put('/events/:id', (req, res) => {
+    // TODO: Create Functionality
+    res.send('Update an event')
+})
+
+// Delete an event
+app.delete('/events/:id', (req, res) => {
+    // TODO: Create Functionality
+    res.send('Delete an event')
+})
+
 export default app
