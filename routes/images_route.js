73c019315f77f67272590216d70b049756d94d65
Finley <trinity.tringl@gmail.com>
2024-02-24 05:05:48 +1000

    fix: fixed uploading pfp

--- routes/images_route.js
+++ routes/images_route.js
@@ -4,67 +4,54 @@ import multer from "multer"
 import { S3Client, PutObjectCommand, GetObjectCommand } from "@aws-sdk/client-s3"
 import dotenv from "dotenv"
 import { User } from "../db.js"
+import { getSignedUrl } from "@aws-sdk/s3-request-presigner"
 
 dotenv.config()
 
-const upload = multer({ dest: 'uploads/' }) // Create a new multer instance
+const storage = multer.memoryStorage()
+const upload = multer({ storage: storage })
 
 const router = Router() // Create a new router
 
 const s3 = new S3Client({ // Create a new S3 client
-    region: process.env.AWS_REGION,
+    region: process.env.PFP_REGION,
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY
+        accessKeyId: process.env.AWS_ACCESS_KEY,
+        secretAccessKey: process.env.AWS_SECRET_KEY
     }
 })
 
 async function uploadToS3(file, userId) {
     const params = {
-        Bucket: process.env.AWS_BUCKET_NAME,
+        Bucket: process.env.PFP_BUCKET,
         Key: file.originalname,
         Body: file.buffer,
         ContentType: file.mimetype,
-        ACL: 'public-read'
     }
-    const command = new PutObjectCommand(params)
-    // store the key in the user's picture field
-    const user = User.findById(userId) 
-    user.picture = file.originalname
-    user.save()
-
-    return await s3.send(command)
+    try {
+        const command = new PutObjectCommand(params)
+        // store the key in the user's picture field
+        const user = await User.findById(userId) 
+        user.picture = file.originalname
+        const url = await getSignedUrl(s3, new GetObjectCommand({ Bucket: process.env.PFP_BUCKET, Key: file.originalname }))
+        user.pictureUrl = url
+        await user.save()
+    
+        return await s3.send(command)
+    } catch (error) {
+        console.error(error)
+    }
 }
 
-async function fetchFromS3(userId) {
-    const user = User.findById(userId)
-    const key = user.picture
-
-    const params = {
-        Bucket: process.env.AWS_BUCKET_NAME,
-        Key: key
-    }
-
-    const command = new GetObjectCommand(params)
-
-    return await s3.send(command)
-}
-
-router.post('/pfp', /* authenticateToken, */ upload.single('image'), async (req, res) => {
+router.post('/pfp', authenticateToken, upload.single('image'), async (req, res) => {
     const userId = req.user._id
     const file = req.file
+
+    console.log(file)
 
     const result = await uploadToS3(file, userId)
 
     res.send(result)
 })
-
-router.get('/pfp', /* authenticateToken, */ async (req, res) => {
-    const userId = req.user._id
-    const file = await fetchFromS3(userId)
-
-    file.pipe(res)
-})
-
 
 export default router
