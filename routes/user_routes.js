6a64f328a388b6218bcb1228324804df8c531c70
Finley <trinity.tringl@gmail.com>
2024-02-23 16:20:10 +1000

    refactor: moved UserClass out

--- routes/user_routes.js
+++ routes/user_routes.js
@@ -1,23 +1,8 @@
 import { Router } from "express"
 import { User } from '../db.js'
+import UserClass from '../models/user.js'
 
 const router = Router()
-
-// User return structure
-class UserClass {
-    constructor(user) {
-        this._id = user._id
-        this.username = user.username
-        this.isOrganiser = user.isOrganiser
-        this.picture = user.picture
-        this.description = user.description
-        this.animes = user.animes
-        this.characters = user.characters
-        this.actors = user.actors
-        this.isAdmin = user.isAdmin
-        this.date_created = user.date_created
-    }
-}
 
 // Search by username, isOrganiser, isAdmin
 router.get('/', async (req, res) => {
